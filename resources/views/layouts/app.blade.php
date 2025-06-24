b0674bc2fd6106b57299be78fb748fa8f1ecc374
abobasel86 <101171178+abobasel86@users.noreply.github.com>
2025-06-25 00:45:40 +0300

    Fix charset meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
     <head>
-        <meta charset="utf-g">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
