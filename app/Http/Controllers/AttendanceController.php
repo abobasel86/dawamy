b3f8d6b5b2bd276c03bb9977bf2e7294cc74677f
abobasel86 <101171178+abobasel86@users.noreply.github.com>
2025-06-30 12:12:34 +0300

    Remove unused WebAuthnCredential import

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\AttendanceLog;
-use Laragear\WebAuthn\Models\WebAuthnCredential;
 use Carbon\Carbon;
 use Webauthn;
 
